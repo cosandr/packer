d8530a3913f5b4635bd0bd59892dd4388454a4b6
Andrei Costescu <andrei@costescu.no>
2021-11-06 22:34:16 +0100

    Fix errors caused by cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf /etc/ssh/ssh_host_*key* /root/* /var/log/* /var/spool/mail/*
+rm -rf /etc/ssh/ssh_host_*key* /root/* /var/spool/mail/*
 rm -f $HISTFILE && unset HISTFILE
 HISTSIZE=0
 
@@ -8,7 +8,7 @@ rm -rf /usr/share/backgrounds/*
 
 yum -y install glibc-locale-source glibc-langpack-en
 rm -f /usr/lib/locale/locale-archive
-/usr/bin/localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
+/usr/bin/localedef -v -c -i en_US -f UTF-8 en_US.UTF-8 &> /dev/null
 
 rpm --rebuilddb
 yum clean all
@@ -20,3 +20,5 @@ for f in "${cleanup[@]}"; do
   dd if=/dev/zero of="$f" bs=1M
   rm -f "$f"
 done
+
+find /var/log -type f -delete
