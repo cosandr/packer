f0155a2c1777aa74bfe4f716456ca0bb5ef30dce
Andrei Costescu <andrei@costescu.no>
2022-04-12 14:14:52 +0200

    Support btrfs rootfs

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,8 +5,6 @@ rm -f $HISTFILE && unset HISTFILE
 HISTSIZE=0
 
 rm -rf /usr/share/backgrounds/*
-
-yum -y install glibc-locale-source glibc-langpack-en
 rm -f /usr/lib/locale/locale-archive
 /usr/bin/localedef -v -c -i en_US -f UTF-8 en_US.UTF-8 &> /dev/null
 
@@ -14,11 +12,22 @@ rpm --rebuilddb
 yum clean all
 rm -rf /var/cache/yum/*
 
-cleanup=( /boot/zero /boot/efi/zero /zero )
+cleanup=( /boot /boot/efi / )
 
-for f in "${cleanup[@]}"; do
-  dd if=/dev/zero of="$f" bs=1M
-  rm -f "$f"
+for d in "${cleanup[@]}"; do
+  if [[ ! -d "$d" ]]; then
+    echo "$d doesn't exist, skip"
+    continue
+  fi
+
+  fs_type="$(findmnt -nrv -o fstype "$d")"
+  if [[ $fs_type = btrfs ]]; then
+    btrfs scrub start -B "$d"
+    btrfs balance start --full-balance "$d"
+  else
+    dd if=/dev/zero of="$d/zero" bs=1M
+    rm -fv "$d/zero"
+  fi
 done
 
 find /var/log -type f -delete
